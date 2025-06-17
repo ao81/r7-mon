c0b07ffe8193c3d2dc0b80261e9efd11b27c61d1
ªº <mc4@outlook.jp>
2025-06-17 23:30:03 +0900

    Create 3-2.c

---
+++ 3-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int wa(int x, int y) {
+        return x + y;
+}
+
+int sa(int x, int y) {
+        return x - y;
+}
+
+int main(void) {
+        int a, b;
+
+        printf("a = ");
+        scanf("%d", &a);
+        printf("b = ");
+        scanf("%d", &b);
+
+        printf("\n和: %d\n", wa(a, b));
+        printf("差: %d\n", sa(a, b));
+        return 0;
+}
