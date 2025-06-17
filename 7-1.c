292209d4603693508a0d44b321d0d58230f22950
ªº <mc4@outlook.jp>
2025-06-17 22:56:34 +0900

    Create 7-1.c

---
+++ 7-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+        int a[10] = {}, i, sum = 0;
+        for(i = 0; i < 10; i++) {
+                printf("[%d] = ", i + 1);
+                scanf("%d", &a[i]);
+                sum += a[i];
+        }
+        printf("合計 = %d, 平均 = %d\n", sum, sum / 10);
+        return 0;
+}
