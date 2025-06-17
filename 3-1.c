1cbeb330f2848603c6a64c579f84e05c1f44eeba
ªº <mc4@outlook.jp>
2025-06-17 23:27:27 +0900

    Create 3-1.c

---
+++ 3-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+        int i = 0;
+
+        while(i < 5) {
+                printf("岡山県立岡山工業高等学校\n");
+                i++;
+        }
+        return 0;
+}
