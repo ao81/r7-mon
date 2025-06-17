0ff8dc85992e6e35441874d87c88ed6571550216
ªº <mc4@outlook.jp>
2025-06-18 07:51:02 +0900

    4-2.c を更新

--- 4-2.c
+++ 4-2.c
@@ -3,13 +3,13 @@
 int main(void) {
     int i, n, sum;
     
-    for(i = 0, i < 3; i++) {
+    for(i = 0; i < 3; i++) {
         printf("Nの値は？ ");
         scanf("%d", &n);
         for(sum = 0; n != 0; n--) {
             sum += n;
         }
-        printf("合計 = %d", sum);
+        printf("合計 = %d\n", sum);
     }
     return 0;
 }
