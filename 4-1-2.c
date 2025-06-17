274640bd28a4076b6d3ed342311791e70f3eb2ed
ªº <mc4@outlook.jp>
2025-06-17 22:54:20 +0900

    Create 4-1-2.c

---
+++ 4-1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+        int n, sum;
+        printf(" n\tsum\n");
+        for(n = 1, sum = 0; sum < 100; n++) {
+                sum += n;
+                printf("%2d\t%3d\n", n, sum);
+        }
+        return 0;
+}
