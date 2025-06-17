eaef1d429de5faf2491c76802709bb26721aaf49
ªº <mc4@outlook.jp>
2025-06-17 23:25:31 +0900

    Create 2-3.c

---
+++ 2-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+        float a, b;
+
+        printf("a = ");
+        scanf("%f", &a);
+        printf("b = ");
+        scanf("%f", &b);
+
+        printf("\n和 = %7.2f\n", a + b);
+        printf("差 = %7.2f\n", a - b);
+        printf("積 = %7.2f\n", a * b);
+        printf("商 = %7.2f\n", a / b);
+
+        return 0;
+}
