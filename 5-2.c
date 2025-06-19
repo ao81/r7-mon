6f3f4c1be4c831769921c39a95a74cc9970698cd
ªº <mc4@outlook.jp>
2025-06-20 07:57:50 +0900

    5-2.c を更新

--- 5-2.c
+++ 5-2.c
@@ -3,8 +3,10 @@
 int main(void) {
         int a, b, max;
         scanf("%d %d", &a, &b);
-        if(a > b) max = a;
-            else max = b;
+        if(a > b) 
+                max = a;
+        else
+                max = b;
         printf("max = %d\n", max);
         return 0;
 }
