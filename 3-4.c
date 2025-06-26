e775b27cc462a2e35f0e77b262997151e4dc4e60
ªº <mc4@outlook.jp>
2025-06-26 17:46:12 +0900

    Create 3-4.c

---
+++ 3-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int gokei(int n) {
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+                sum += i++;
+        }
+        return sum;
+}
+
+int main() {
+        int n;
+        printf("n = ");
+        scanf("%d", &n);
+        int sum = gokei(n);
+        printf("合計: %d\n", sum);
+
+        return 0;
+}
