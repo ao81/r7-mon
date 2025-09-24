c15f9388417708689ae00440ae88501c9c0440e4
ªº <mc4@outlook.jp>
2025-09-24 22:29:34 +0900

    Update 4-2.c

--- 4-2.c
+++ 4-2.c
@@ -2,14 +2,15 @@
 
 int main(void) {
     int i, n, sum;
-    
-    for(i = 0; i < 3; i++) {
-        printf("Nの値は？ ");
+
+    for (i = 0; i < 3; i++) {
+        printf("Nの値は? ");
         scanf("%d", &n);
-        for(sum = 0; n != 0; n--) {
+        for (sum = 0; n > 0; n--) {
             sum += n;
         }
         printf("合計 = %d\n", sum);
     }
+
     return 0;
 }
