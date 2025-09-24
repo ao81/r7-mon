369330967c734fcead7cdcc65dabfef4aaa08da0
ªº <mc4@outlook.jp>
2025-09-24 22:27:07 +0900

    Update 4-1-2.c

--- 4-1-2.c
+++ 4-1-2.c
@@ -2,10 +2,12 @@
 
 int main(void) {
         int n, sum;
+
         printf(" n\tsum\n");
-        for(n = 1, sum = 0; sum < 100; n++) {
+        for (n = 1, sum = 0; sum < 100; n++) {
                 sum += n;
                 printf("%2d\t%3d\n", n, sum);
         }
+
         return 0;
 }
