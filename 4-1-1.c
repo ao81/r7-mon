2612bc384cc66ca3bce89e28de8db4f49e2aca73
ªº <mc4@outlook.jp>
2025-09-24 22:26:38 +0900

    Update 4-1-1.c

--- 4-1-1.c
+++ 4-1-1.c
@@ -2,8 +2,10 @@
 
 int main(void) {
         int i;
-        for(i = 0; i < 5; i++) {
+
+        for (i = 0; i < 5; i++) {
                 printf("岡山県立岡山工業高等学校\n");
         }
+
         return 0;
 }
