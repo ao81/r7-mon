01a7b05a8c0b29b2a58730ffaa44fd8f92f23dbf
ªº <mc4@outlook.jp>
2025-09-26 08:38:00 +0900

    Create 4-3.c

---
+++ 4-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, n, sum = 0, ave;
+
+	for(i = 0; i < 10; i++) {
+		printf("%d回目の入力: ", i + 1);
+		scanf("%d", &n);
+		sum += n;
+	}
+	printf("合計 = %d\n", sum);
+	ave = sum / 10;
+	printf("平均 = %d\n", ave);
+	
+	return 0;
+}
