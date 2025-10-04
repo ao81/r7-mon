62f1f75281bd84b459056b86e33d34d85fe984d2
ªº <mc4@outlook.jp>
2025-10-04 17:41:37 +0900

    Update 8-2.c

--- 8-2.c
+++ 8-2.c
@@ -2,25 +2,29 @@
 
 int main(void) {
 	int ten[5][3], i, j, sum;
+	float ave;
 
 	for (i = 0; i < 5; i++) {
 		printf("生徒%dの得点を入力してください。\n", i + 1);
 		for (j = 0; j < 3; j++) {
-			if (j == 0) {
-				printf("国語: ");
-				scanf("%d", &ten[i][j]);
-			} else if (j == 1) {
-				printf("数学: ");
-				scanf("%d", &ten[i][j]);
-			} else {
-				printf("英語: ");
-				scanf("%d", &ten[i][j]);
+			switch(j) {
+				case 0:
+					printf("国語: ");
+					break;
+				case 1:
+					printf("数学: ");
+					break;
+				case 2:
+					printf("英語: ");
+					break;
 			}
+			scanf("%d", &ten[i][j]);
 		}
 	}
 
-	printf("--------得点--------\n");
-	printf("      国語 数学 英語   合計 平均\n");
+	printf("\n");
+	printf("---------------得点---------------\n");
+	printf("      国語 数学 英語   合計  平均\n");
 	for (i = 0; i < 5; i++) {
 		sum = 0;
 		printf("生徒%d ", i + 1);
@@ -28,9 +32,8 @@ int main(void) {
 			printf("%4d ", ten[i][j]);
 			sum += ten[i][j];
 		}
-		printf("   %d   %d", sum, sum / 3);
+		ave = sum / 3.0;
+		printf("  %4d  %3.1f", sum, ave);
 		printf("\n");
 	}
-	
-	return 0;
 }
