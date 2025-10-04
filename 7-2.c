1c0e306635a243f4df31e4c919d29f39c4b4b6c4
ªº <mc4@outlook.jp>
2025-10-04 11:02:00 +0900

    Update 7-2.c

--- 7-2.c
+++ 7-2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-	int b[10] = {}, i, n;
+	int i, n, b[10] = {0};
 
-	printf("０～９の値を１０回入力してください。\n");
+	printf("0~9の値を10回入力してください。\n");
 	for (i = 0; i < 10; i++) {
 		scanf("%d", &n);
+		if (n < 0 || n >= 10) return 1;
 		b[n]++;
 	}
 
-	for (i = 0; i < 10; i++) {
-		printf("b[%d] = %d\n", i, b[i]);
-	}
-	
+	printf("\n");
+	for (i = 0; i < 10; i++) printf("[%d]: %d\n", i, b[i]);
+
 	return 0;
 }
