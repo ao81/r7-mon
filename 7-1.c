94e99cef21a4a737b93ee1f3cdadac03acd572a3
ªº <mc4@outlook.jp>
2025-10-04 10:53:27 +0900

    Update 7-1.c

--- 7-1.c
+++ 7-1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[10], sum = 0, ave;
+	int i, a[10], sum = 0, ave;
 
-	for (int i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++) {
 		printf("a[%d] = ", i);
 		scanf("%d", &a[i]);
 	}
 
-	for (int i = 0; i < 10; i++) sum += a[i];
+	for (i = 0; i < 10; i++) sum += a[i];
 	ave = sum / 10;
 
 	printf("\n合計: %d\n平均: %d\n", sum, ave);
